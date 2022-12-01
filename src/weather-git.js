b7bfcac881acc44f66bf37a344ac2ea3618a17dd
C0URTN3Y4 <115426619+C0URTN3Y4@users.noreply.github.com>
2022-12-01 16:51:02 -0500

    Adjustments made to search bar code

--- src/weather-git.js
+++ src/weather-git.js
@@ -70,7 +70,7 @@ function condtions(response) {
 function cityInput(event) {
   event.preventDefault();
   let apiKey = `8cac06f7ab6c10287cd06a316ff84a57`;
-  let city = document.querySelector("#search").value;
+  let city = document.querySelector("#name").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(`${apiUrl}`).then(condtions);
 }
